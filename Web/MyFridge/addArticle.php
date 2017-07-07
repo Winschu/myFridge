0bf50fd26c967246c12f09f8ffbb7f29010307fe
Winschu <marcellohn@msn.com>
2017-07-07 23:14:59 +0200

    Added serializeArray to addArticle

--- Web/MyFridge/addArticle.php
+++ Web/MyFridge/addArticle.php
@@ -7,11 +7,19 @@ require_once("includes/loadAssets.html");
         $(document).ready(function () {
             getArticleGroupList();
             getProducerNameList();
-
             var formArray = $("#newArticleForm").serializeArray();
 
-            $("#submitFormButton").click(function () {
 
+            $("#submitFormButton").click(function () {
+                var newArticle = {
+                    name: formArray[0].value,
+                    articleGroup: formArray[1].value,
+                    producer : formArray[2].value,
+                    barcode : formArray[3].value,
+                    size : formArray[4].value,
+                    sizeType : formArray[5].value
+                };
+                insertNewArticle(newArticle);
             });
 
         });
@@ -30,19 +38,22 @@ require_once("includes/loadAssets.html");
                 <div class="d-flex flex-wrap justify-content-center">
                     <div class="p-12">
                         Name
-                        <input type="text" class="form-control" id="nameInput" placeholder="Artikelname eintragen...">
+                        <input type="text" class="form-control" name="nameInput" id="nameInput"
+                               placeholder="Artikelname eintragen...">
                     </div>
                 </div>
                 <div class="d-flex flex-wrap justify-content-around">
                     <div class="p-6">
                         Artikelgruppe
-                        <select class="form-control" id="articleGroupSelect" title="articleGroupSelect">
+                        <select class="form-control" name="articleGroupSelect" id="articleGroupSelect"
+                                title="articleGroupSelect">
                             <option selected disabled>Bitte auswählen...</option>
                         </select>
                     </div>
                     <div class="p-6">
                         Hersteller
-                        <select class="form-control" id="producerNameSelect" title="producerNameSelect">
+                        <select class="form-control" name="producerNameSelect" id="producerNameSelect"
+                                title="producerNameSelect">
                             <option selected disabled>Bitte auswählen...</option>
                         </select>
                     </div>
@@ -50,17 +61,19 @@ require_once("includes/loadAssets.html");
                 <div class="d-flex flex-wrap justify-content-around">
                     <div class="p-12">
                         Barcode
-                        <input type="number" class="form-control" id="barcodeInput" placeholder="Barcode eintragen...">
+                        <input type="number" class="form-control" name="barcodeInput" id="barcodeInput"
+                               placeholder="Barcode eintragen...">
                     </div>
                 </div>
                 <div class="d-flex flex-wrap justify-content-center">
                     <div class="p-6">
                         Größe
-                        <input type="number" class="form-control" id="sizeInput" placeholder="Größe eintragen...">
+                        <input type="number" class="form-control" name="sizeInput" id="sizeInput"
+                               placeholder="Größe eintragen...">
                     </div>
                     <div class="p-6">
                         Einheit
-                        <select class="form-control" id="sizeTypeSelect" title="sizeTypeSelect">
+                        <select class="form-control" name="sizeTypeSelect" id="sizeTypeSelect" title="sizeTypeSelect">
                             <option value="g">g</option>
                             <option value="kg">kg</option>
                             <option value="ml">ml</option>
