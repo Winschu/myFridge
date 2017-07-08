4c6e4fb5d6a7ae0fbd20923252c7ca05403465ca
Freigeist <admin@white-it.net>
2017-07-08 15:41:47 +0200

    Changed Database class

--- Web/MyFridge/includes/Database.php
+++ Web/MyFridge/includes/Database.php
@@ -20,4 +20,28 @@ class Database {
 		}
 		return hash("sha256", $pass) == $res["password_hash"];
 	}
+
+	public function newUser(string $user, string $pass, string $email){
+		$hash = hash("sha256", $pass);
+		if(!(pg_prepare($this->con,"", "INSERT INTO user_data (user_name, email, password_hash) VALUES $1, $2, $3"))){
+			throw new ErrorException(pg_last_error());
+		}
+		if(!($dbres = pg_execute($this->con, "", array($user, $email, $hash)))) {
+			throw new Exception(pg_last_error());
+		}
+		return pg_affected_rows($this->con);
+	}
+
+	public function changePassword(string $user, string $newPass){
+		$hash = hash("sha256", $newPass);
+		if(!(pg_prepare($this->con,"", "UPDATE user_data SET password_hash = $1 WHERE user_name LIKE $2"))){
+			throw new ErrorException(pg_last_error());
+		}
+		if(!($dbres = pg_execute($this->con, "", array($hash, $user)))) {
+			throw new Exception(pg_last_error());
+		}
+		return pg_affected_rows($this->con);
+	}
+
+
 }
